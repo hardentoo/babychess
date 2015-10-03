5bb3ffa2b46bf68a52aac486978a2ab6698c8d22
Steven Shaw <steven@steshaw.org>
2015-10-03 12:32:40 +1000

    movePos

--- babychess.hs
+++ babychess.hs
@@ -25,8 +25,8 @@ newtype Piece = Piece (PieceColor, PieceType)
   deriving (Show, Eq)
 
 data State = State {
-  turn :: PieceColor,
-  board :: Board
+  stateTurn :: PieceColor,
+  stateBoard :: Board
 }
 
 prettyType :: PieceType -> String
@@ -64,13 +64,39 @@ valuePiece (Piece (White, typ)) = valuePieceType typ
 evaluateBoard :: Board -> Int
 evaluateBoard (Board rows) = sum $ map evaluateRow rows
   where evaluateRow row = sum $ map evaluateSquare row
-        evaluateSquare (Square (Nothing)) = 0
+        evaluateSquare (Square Nothing) = 0
         evaluateSquare (Square (Just piece)) = valuePiece piece
 
-xx = Square Nothing
+--
+-- FIX moving is very inefficient. Use vectors?
+--
+
+updateItem :: [x] -> x -> Int -> [x]
+updateItem [] _ _ = []
+updateItem (x:xs) y index = if index == 0 
+                            then y:xs
+                            else x:updateItem xs y (index - 1)
+
+updateSquare :: Board -> Square -> Pos -> Board
+updateSquare (Board squares) s (Pos (x, y)) = Board $ updateItem squares newRow y
+  where newRow = updateItem (squares !! y) s x
+
+squareAt :: Board -> Pos -> Square
+squareAt (Board squares) (Pos (x, y)) = squares !! y !! x
+
+movePos :: Pos -> Pos -> Board -> Board
+movePos oldPos newPos board = updateSquare board1 (squareAt board oldPos) newPos
+ where board1 = updateSquare board (Square Nothing) oldPos
+
+black :: PieceType -> Square
 black p = Square $ Just $ Piece (Black, p)
+
+white :: PieceType -> Square
 white p = Square $ Just $ Piece (White, p)
 
+xx,bk,bq,bb,bn,br,bp,wk,wq,wb,wn,wr,wp :: Square
+
+xx = Square Nothing
 bk = black King
 bq = black Queen
 bb = black Bishop
@@ -85,17 +111,23 @@ wn = white Knight
 wr = white Rook
 wp = white Pawn
 
+initialBoard :: Board
 initialBoard = Board
-  [[br,bn,bb,bk,bq,bb,bn,br]
+  [[br,bn,bb,bq,bk,bb,bn,br]
   ,[bp,bp,bp,bp,bp,bp,bp,bp]
   ,[xx,xx,xx,xx,xx,xx,xx,xx]
   ,[xx,xx,xx,xx,xx,xx,xx,xx]
   ,[xx,xx,xx,xx,xx,xx,xx,xx]
   ,[xx,xx,xx,xx,xx,xx,xx,xx]
   ,[wp,wp,wp,wp,wp,wp,wp,wp]
-  ,[wr,wn,wb,wk,wq,wb,wn,wr]
+  ,[wr,wn,wb,wq,wk,wb,wn,wr]
   ]
 
+-- e2-e4
+move1 :: Board
+move1 = movePos (Pos (4,6)) (Pos (4, 4)) initialBoard
+
+whiteWinningBoard :: Board
 whiteWinningBoard = Board
   [[bk,xx,xx,xx,xx,xx,xx,xx]
   ,[bp,bp,xx,xx,xx,xx,xx,xx]
@@ -107,13 +139,16 @@ whiteWinningBoard = Board
   ,[xx,wk,xx,wr,xx,xx,xx,xx]
   ]
 
-printBoard b = do
+printBoard :: String -> Board -> IO ()
+printBoard name b = do
+  putStrLn ""
+  putStrLn name
   putStr $ prettyBoard b
   putStrLn $ "evaluation: " ++ show (evaluateBoard b)
 
-main = do 
-  putStrLn "babychess 0.0\n"
-  printBoard initialBoard
-  putStrLn ""
-  printBoard whiteWinningBoard
-  putStrLn ""
+main :: IO ()
+main =
+  putStrLn "babychess 0.0\n" >>
+  printBoard "initial" initialBoard >>
+  printBoard "e2-e4" move1 >>
+  printBoard "white winning" whiteWinningBoard
